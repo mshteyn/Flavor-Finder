82fb64d85d54c5b963a6596c53e9a9da924bb007
William Porteous <afpwilliam@gmail.com>
2024-05-20 20:41:26 -0500

    prepared files as csv / json; beginning to explore data

--- examine_data.py
+++ examine_data.py
@@ -1,1 +1,21 @@
 #file by Will to examine the dataset and manipulate it
+
+import pandas as pd 
+
+
+
+
+
+if __name__ == "__main__":
+    
+    reviews_path  = "reviews_kcore_json"
+    metadata = "metadata_kcore_csv"
+    #metadata = pd.read_csv(metadata_kcore_csv)
+    #reviews = pd.read_json(reviews_kcore_json)
+
+
+
+
+
+
+
--- getdata.sh
+++ getdata.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 echo "downloading reviews" 
 
-curl -o reviews_kcore.json https://datarepo.eng.ucsd.edu/mcauley_group/gdrive/googlelocal/review-California_10.json.gz
+curl -o reviews_kcore_json.gz https://datarepo.eng.ucsd.edu/mcauley_group/gdrive/googlelocal/review-California_10.json.gz
 
 echo "review download complete; downloading metadata"
 
-curl -o metadata_kcore.csv https://datarepo.eng.ucsd.edu/mcauley_group/gdrive/googlelocal/rating-California.csv.gz
+curl -o metadata_kcore_csv.gz https://datarepo.eng.ucsd.edu/mcauley_group/gdrive/googlelocal/rating-California.csv.gz
 
 echo "metadata download complete" 
+
+#Unzip both files 
+
+gunzip metdata_kcore_csv.gz 
+gunzip reviews_kcore_json.gz 
