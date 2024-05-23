af8a2ef10bd457ccf9ab519560e0c20fd1255c34
misha <misha@mishas-mbp.mynetworksettings.com>
2024-05-23 03:50:29 -0400

    retreival from vector DB active

--- LC_test.py
+++ LC_test.py
@@ -2,6 +2,8 @@ import pdb
 import torch
 from langchain import HuggingFacePipeline
 from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig, pipeline
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -19,12 +21,13 @@ if torch.cuda.is_available():
 	model = AutoModelForCausalLM.from_pretrained(
 
 	    MODEL_NAME, torch_dtype=torch.float16, trust_remote_code=True, device_map="auto")
-
+	device = torch.device("cuda")
 	print('Cuda is Available. Model is: ', MODEL_NAME)
 else:
 
 	#MODEL_NAME = "TheBloke/Llama-2-7B-GGML"
 	pdb.set_trace()	
+	device = torch.device("cpu")	
 	MODEL_NAME = "HirCoir/TinyLlama-1.1B-Chat-v1.0-GGUF" 
 	from ctransformers import AutoModelForCausalLM
 	llm = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
@@ -51,18 +54,29 @@ llm = HuggingFacePipeline(pipeline=text_pipeline, model_kwargs={"temperature": 0
 
 from langchain import PromptTemplate
 
-instruction = input("What kind of food recommendation would you like?:  ")
+query = input("\nWhat kind of food recommendation would you like?:  ")
  
-#template = """
-#<s>[INST] <<SYS>>
-#Act as a Server giving specific and concise restaurant recommendations
-#<</SYS>>
-# 
-#{text} [/INST]
-#"""
 
-extra_instruction = " in just two sentences, maximum. Please use the following context in generating your answer:"
-template = """<s>[INST] <<SYS>>"""+instruction+extra_instruction+"""<</SYS>>{text} [/INST]"""
+# EMBEDDING MODEL
+model_name  = 'Alibaba-NLP/gte-large-en-v1.5'
+embedding_function = HuggingFaceEmbeddings(model_name=model_name, model_kwargs = {'trust_remote_code':True, 'device': device})
+#load Vector DB
+chroma_dir = './PA_Full_named_db'#once its ready..
+chroma_dir = './test_PA_db' #for now
+db = Chroma(persist_directory=chroma_dir, embedding_function=embedding_function)
+
+docs = db.similarity_search(query, k=5)
+context = ''
+
+print('\nThe most relevant reviews are: \n')
+for i, doc in enumerate(docs):
+	print('Review #', i+1, doc.page_content)
+	context += 'Reviewer ' + str(i+1) + ' says: ' +  doc.page_content + '. '
+
+
+extra_instruction = ". Answer in just two sentences, maximum. Use the following context in generating your answer:"
+
+template = """<s>[INST] <<SYS>>"""+query+extra_instruction+"""<</SYS>>{text} [/INST]"""
 
  
 prompt = PromptTemplate(
@@ -70,9 +84,11 @@ prompt = PromptTemplate(
     template=template,
 )
 
-context = input("What other reviewers are saying: ")
 
+#context = input("What other reviewers are saying: ")
 #text = "Give me a recommendation for light dish at Apteka"
+
+
 print(llm(prompt.format(text=context)))
 
 pdb.set_trace()
@@ -83,13 +99,3 @@ pdb.set_trace()
 #print(result)
 
 
-
-
-
-
-
-
-
-
-
-pdb.set_trace()
