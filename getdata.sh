b78626a8be04cbcb22e7b04508eed9f865963310
William Porteous <afpwilliam@gmail.com>
2024-05-25 13:35:05 -0500

    file processing data to get url eventually

--- getdata.sh
+++ getdata.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+
+
 echo "downloading reviews" 
 
-curl -o reviews_kcore_json.gz https://datarepo.eng.ucsd.edu/mcauley_group/gdrive/googlelocal/review-California_10.json.gz
+curl -o reviews_cali_json.gz https://datarepo.eng.ucsd.edu/mcauley_group/gdrive/googlelocal/review-California.json.gz
 
 echo "review download complete; downloading metadata"
 
-curl -o metadata_kcore_csv.gz https://datarepo.eng.ucsd.edu/mcauley_group/gdrive/googlelocal/rating-California.csv.gz
+curl -o metadata_cali_json.gz https://datarepo.eng.ucsd.edu/mcauley_group/gdrive/googlelocal/meta-California.json.gz
 
 echo "metadata download complete" 
 
 #Unzip both files 
 echo "unzipping metadata"
-gunzip metadata_kcore_csv.gz 
+
+gunzip metadata_cali_json.gz
+
 echo "unzipping reviews"
-gunzip reviews_kcore_json.gz 
+
+gunzip reviews_cali_json.gz 
